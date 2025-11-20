e853b3c4332502764b78ab54965e78b05c599817
TormodEngen <tormodengen24@gmail.com>
2025-11-20 10:50:51 +0100

    Fiks på PID og oppsett av pådragsberegning

--- NodeCode/Src/control_card.c
+++ NodeCode/Src/control_card.c
@@ -34,24 +34,32 @@ void PID_calculation(void){
 	float a;					// Filter parameter
 	float ymf;					// Filter quantity to D-ledd
 	float ymf_past = 0;
-	float Tf = (1.0/(2*3));		// Time frequancy
+	float Tf = (1.0/(2*3.14));		// Time frequancy
 	float Ts = Tf/5;
-	uint16_t ui_max = 100;
-	uint16_t u_max = 100;
-	uint8_t kp = 1;				// Proportinal parameter
-	uint8_t Ti = 0;				// Integrator parameter
-	uint8_t Td = 0;				// Derivator parameter
+	int16_t ui_max = 65535;
+	int16_t u_max = 65532;
+	float kp = 1;				// Proportinal parameter
+	float Ti = 0;				// Integrator parameter
+	float Td = 0;				// Derivator parameter
 	uint16_t yr = 5;			// Reference
 	uint16_t ym = 2;			// Actual placement
 
 
 	error = yr-ym;
+	if (error <= 5){
+		error = 0;
+	}
 
 	a = Tf/(Tf+Ts);											// Filter parameter
 
 	up = kp*error;											// P-Ledd
 
-	ui = ui_past + (kp*Ts*(error-error_past))/(Ti*2);		// I-Ledd
+	if (Ti == 0){
+		ui = 0;
+	}
+	else{
+		ui = ui_past + (kp*Ts*(error-error_past))/(Ti*2);	// I-Ledd
+	}
 	if (ui >= ui_max){										// I-ledd restriction
 		ui = ui_max;
 	}
@@ -69,14 +77,13 @@ void PID_calculation(void){
 	ui_past = ui;
 	ymf_past = ymf;
 
-	return u;
 }
 
 // Calculating the different variables to change TIM3
 void power_delivery(void){
 
 	prescalar = 71;				// Gives 1 MHz = 1 us if 71
-	period = 0; 				// Needs a frequency at 0-250kHz: 4 gives 250kHz and bigger gives less and less
+	period = ~u; 				// Needs a frequency at 0-250kHz: 3 gives 250kHz and bigger gives less and less
 	puls = (period+1)/2;		// Needs to be half of the period
 }
 
